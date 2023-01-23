d3c3de0bd0972e38735eee89b6364c5b782c6d82
AishathRafahath <aishathrafahath@gmail.com>
2023-01-23 12:39:12 +0500

    will you commit this

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let addExpenses = document.getElementById('add_Expenses');
 let thetable = document.getElementById('table');
 
 let dataArray = []
+console.log('yo');
 
 addExpenses.addEventListener("click",function(e){
     e.preventDefault();
@@ -27,22 +28,37 @@ addExpenses.addEventListener("click",function(e){
 function render(object){
     for(let i=0; i<1; i++){
        let row = document.createElement("tr");
-       let nameCell = document.createElement("th")
+       let nameCell = document.createElement("th");
             nameCell.textContent = object.name;
-       let dateCell = document.createElement("th")
+       let dateCell = document.createElement("th");
             dateCell.textContent = object.date;
-       let amountCell = document.createElement("th")
+       let amountCell = document.createElement("th");
            amountCell.textContent = object.amount;
+        let delCell = document.createElement("th");
+        let delBtn = document.createElement("button")
+            delBtn.textContent = "x"
+            delBtn.className = "del"
+            delBtn.addEventListener("click",function(){
+                deleteRow()
+            })
+
+
 
        row.appendChild(nameCell);
        row.appendChild(dateCell);
        row.appendChild(amountCell);
+       delCell.appendChild(delBtn);
+       row.appendChild(delCell);
        console.log('no');
 
         thetable.appendChild(row);
 
         console.log('howdy');
     }
+}
+
+function deleteRow(){
+
 }
 
 

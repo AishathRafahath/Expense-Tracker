b1851c610d5425f1f4dd563a2607a1b742ad9535
AishathRafahath <aishathrafahath@gmail.com>
2023-01-25 09:42:21 +0500

    vscode

--- app.js
+++ app.js
@@ -1,67 +1,23 @@
+const nameInput = document.getElementById('name_input');
+const dateInput = document.getElementById('date_input');
+const amountInput = document.getElementById('amount_input');
+const addExpenses = document.getElementById('add_Expenses');
+let expenseArray = [];
+// document.getElementById('name_input');
+// document.getElementById('name_input');
 
-let nameInput = document.getElementById('name_input');
-let dateInput = document.getElementById('date_input');
-let amountInput = document.getElementById('amount_input');
-let addExpenses = document.getElementById('add_Expenses');
-let thetable = document.getElementById('table');
-let count = -1;
-let dataArray = []
-let collectedData = {};
+/* works */
 
-addExpenses.addEventListener("click",function(e){
-    e.preventDefault();
-    collectedData = {
+addExpenses.addEventListener("click",function(){
+    let dataObject = {
         name: nameInput.value,
         date: dateInput.value,
         amount: amountInput.value
     }
-    dataArray.push(collectedData);
-    nameInput.value = ""
-    dateInput.value = ""
-    amountInput.value = ""
-    render(dataArray);
 
-
+    expenseArray.push(dataObject);
+    console.log(dataObject)
 })
 
-function render(object){
 
-
-    for(let i=0; i<1; i++){
-        count ++;
-       let row = document.createElement("tr");
-       row.setAttribute("id",`row${count}`)
-       let nameCell = document.createElement("th");
-            nameCell.textContent = object.name;
-       let dateCell = document.createElement("th");
-            dateCell.textContent = object.date;
-       let amountCell = document.createElement("th");
-           amountCell.textContent = object.amount;
-        let delCell = document.createElement("th");
-        let delBtn = document.createElement("button")
-            delBtn.textContent = "x"
-            delBtn.className = "del"
-
-            delBtn.addEventListener("click",function(){
-                return function deleteRow(count){
-                    let idkwhatimdoing = document.getElementById(`row${count}`)
-                    idkwhatimdoing.remove()
-
-                }
-            })
-
-        // console.log(count);
-        console.log(dataArray)
-
-       row.appendChild(nameCell);
-       row.appendChild(dateCell);
-       row.appendChild(amountCell);
-       delCell.appendChild(delBtn);
-       row.appendChild(delCell);
-       thetable.appendChild(row);
-
-    }
-}
-// button removes item from array
-// displays new array on page
 
